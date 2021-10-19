c85053c0a26a605d9b1a5e44b51218d882906f38
RuslanK <ruslank@example.com>
2021-10-19 16:18:28 +0300

    Resolve #2

--- Tables/dbo.table1.sql
+++ Tables/dbo.table1.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[table1] (
   [id] [int] NULL,
   [col1] [varchar](50) NULL,
-  [col2] [varchar](50) NULL
+  [col2] [varchar](50) NULL,
+  [col3] [varchar](50) NULL
 )
 ON [PRIMARY]
 GO
