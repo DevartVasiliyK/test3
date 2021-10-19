255f05c0b91ea446e5dd03187ffdccb3e68876bd
RuslanK <ruslank@example.com>
2021-10-19 16:19:22 +0300

    Fixed #2

--- Tables/dbo.table1.sql
+++ Tables/dbo.table1.sql
@@ -2,7 +2,8 @@
   [id] [int] NULL,
   [col1] [varchar](50) NULL,
   [col2] [varchar](50) NULL,
-  [col3] [varchar](50) NULL
+  [col3] [varchar](50) NULL,
+  [col4] [varchar](50) NULL
 )
 ON [PRIMARY]
 GO
