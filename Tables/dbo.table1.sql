68ff8baa62dc57c0349712f7a4393eac7019abea
RuslanK <ruslank@example.com>
2021-10-19 16:20:14 +0300

    Fix

--- Tables/dbo.table1.sql
+++ Tables/dbo.table1.sql
@@ -3,7 +3,8 @@
   [col1] [varchar](50) NULL,
   [col2] [varchar](50) NULL,
   [col3] [varchar](50) NULL,
-  [col4] [varchar](50) NULL
+  [col4] [varchar](50) NULL,
+  [col5] [varchar](50) NULL
 )
 ON [PRIMARY]
 GO
